4dced916a973f7f752df4e694114f5c0efc0eaa2
Akira <2273076505@qq.com>
2021-12-27 10:46:59 +0800

    和题目一样的
    不太喜欢keydown，反应和逻辑上不相符

---
+++ vb/program/41_form5_11_2021_12_25/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(57, 31)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(1347, 323)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(723, 373)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(142, 42)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(572, 385)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(494, 388)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 12)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1457, 440)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ vb/program/41_form5_11_2021_12_25/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+    Dim row, layer, a(row, layer) As Long
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button2.Text = "退出"
+        Label1.Text = "请输入"
+    End Sub
+
+    'Private Sub Button1_Click(sender As Object, e As EventArgs)
+    '    row = Val(InputBox("请输入行数", "输入", "3"))
+    '    layer = row
+    '    ReDim a(row, layer)
+    '    TextBox1.Text = ""
+    '    If row > 26 Then
+    '        TextBox1.Text = "最大26行"
+    '    Else
+    '        For i = 1 To row
+    '            For j = 1 To i
+    '                If i = 1 Then
+    '                    a(i, j) = 1
+    '                    TextBox1.Text = TextBox1.Text & a(i, j) & vbTab '" "
+    '                Else
+    '                    a(i, j) = a(i - 1, j - 1) + a(i - 1, j)
+    '                    TextBox1.Text = TextBox1.Text & a(i, j) & vbTab '" "
+    '                End If
+    '            Next
+    '            TextBox1.Text = TextBox1.Text & vbCrLf
+    '        Next
+    '    End If
+    'End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub TextBox2_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox2.KeyDown
+        row = Val(TextBox2.Text)
+        layer = row
+        ReDim a(row, layer)
+        TextBox1.Text = ""
+        If row > 26 Then
+            TextBox1.Text = "最大26行" '只是因为大了的话文本显示不完了，设置一个大小而已
+        Else
+            For i = 1 To row
+                For j = 1 To i
+                    If i = 1 Then
+                        a(i, j) = 1
+                        TextBox1.Text = TextBox1.Text & a(i, j) & vbTab '" "
+                    Else
+                        a(i, j) = a(i - 1, j - 1) + a(i - 1, j)
+                        TextBox1.Text = TextBox1.Text & a(i, j) & vbTab '" "
+                    End If
+                Next
+                TextBox1.Text = TextBox1.Text & vbCrLf
+            Next '判断和输出
+        End If '有同学反应没反应，那是因为你输入就触发，但是文本还没输入进去，输入了文本后这个过程已经结束了，解决办法就是在结束之后加一个回车，不改变内容，但是再次触发控件
+    End Sub
+End Class
