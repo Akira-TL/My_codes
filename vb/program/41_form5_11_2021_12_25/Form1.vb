18ceb69be3387c17a77e00520c520910626d3c01
Akira <2273076505@qq.com>
2021-12-27 11:07:43 +0800

    再补了补

--- vb/program/41_form5_11_2021_12_25/Form1.vb
+++ vb/program/41_form5_11_2021_12_25/Form1.vb
@@ -34,25 +34,51 @@
     End Sub
 
     Private Sub TextBox2_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox2.KeyDown
-        row = Val(TextBox2.Text)
-        layer = row
-        ReDim a(row, layer)
+        Dim n As Integer
+        Dim a As Long(,)
+        Dim i As Integer, j As Integer
+        If e.KeyValue <> 13 Or Not IsNumeric(TextBox2.Text) Then Exit Sub
+        n = Val(TextBox2.Text)
+        ReDim a(n, n)
+        For i = 1 To n
+            a(i, 1) = 1
+            a(i, i) = 1
+        Next i
+        For i = 3 To n
+            For j = 2 To i - 1
+                a(i, j) = a(i - 1, j - 1) + a(i - 1, j)
+            Next j
+        Next
         TextBox1.Text = ""
-        If row > 26 Then
-            TextBox1.Text = "最大26行" '只是因为大了的话文本显示不完了，设置一个大小而已
-        Else
-            For i = 1 To row
-                For j = 1 To i
-                    If i = 1 Then
-                        a(i, j) = 1
-                        TextBox1.Text = TextBox1.Text & a(i, j) & vbTab '" "
-                    Else
-                        a(i, j) = a(i - 1, j - 1) + a(i - 1, j)
-                        TextBox1.Text = TextBox1.Text & a(i, j) & vbTab '" "
-                    End If
-                Next
-                TextBox1.Text = TextBox1.Text & vbCrLf
-            Next '判断和输出
-        End If '有同学反应没反应，那是因为你输入就触发，但是文本还没输入进去，输入了文本后这个过程已经结束了，解决办法就是在结束之后加一个回车，不改变内容，但是再次触发控件
+        For i = 1 To n
+            TextBox1.Text = TextBox1.Text & Space(3 * (n + 1 - i))
+            For j = 1 To i
+                TextBox1.Text = TextBox1.Text & a(i, j) & "     "
+            Next
+            TextBox1.Text = TextBox1.Text & vbCrLf
+        Next
+        TextBox2.Text = "" '书上的代码，利用的字符ASCII码，也可以看看，后面注释掉的是我自己写的，毕竟书上的终归不一定最好
+        '有同学反应没反应，那是因为你输入就触发，但是文本还没输入进去，输入了文本后这个过程已经结束了，解决办法就是在结束之后加一个回车，不改变内容，但是再次触发控件
+        '---------------------------------------------------------------------------------------------------------------------------------------------------------------
+        'row = Val(TextBox2.Text)
+        'layer = row
+        'ReDim a(row, layer)
+        'TextBox1.Text = ""
+        'If row > 26 Then
+        '    TextBox1.Text = "最大26行" '只是因为大了的话文本显示不完了，设置一个大小而已
+        'Else
+        '    For i = 1 To row
+        '        For j = 1 To i
+        '            If i = 1 Then
+        '                a(i, j) = 1
+        '                TextBox1.Text = TextBox1.Text & a(i, j) & vbTab '" "
+        '            Else
+        '                a(i, j) = a(i - 1, j - 1) + a(i - 1, j)
+        '                TextBox1.Text = TextBox1.Text & a(i, j) & vbTab '" "
+        '            End If
+        '        Next
+        '        TextBox1.Text = TextBox1.Text & vbCrLf
+        '    Next '判断和输出
+        'End If '有同学反应没反应，那是因为你输入就触发，但是文本还没输入进去，输入了文本后这个过程已经结束了，解决办法就是在结束之后加一个回车，不改变内容，但是再次触发控件
     End Sub
 End Class
