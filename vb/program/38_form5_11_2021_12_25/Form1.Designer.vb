338eb63516522f5b582e82b42c1c6775643db1ea
Akira <2273076505@qq.com>
2021-12-27 09:51:54 +0800

    最后一题
    补充了最后一个题目

--- vb/program/38_form5_11_2021_12_25/Form1.Designer.vb
+++ vb/program/38_form5_11_2021_12_25/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form 重写 Dispose，以清理组件列表。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     '注意: 以下过程是 Windows 窗体设计器所必需的
     '可以使用 Windows 窗体设计器修改它。  
     '不要使用代码编辑器修改它。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
@@ -32,12 +32,14 @@ Partial Class Form1
         Me.TextBox1.Location = New System.Drawing.Point(57, 31)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(683, 295)
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(1347, 323)
         Me.TextBox1.TabIndex = 0
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(181, 367)
+        Me.Button1.Location = New System.Drawing.Point(544, 373)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(142, 42)
         Me.Button1.TabIndex = 1
@@ -46,7 +48,7 @@ Partial Class Form1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(466, 367)
+        Me.Button2.Location = New System.Drawing.Point(723, 373)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(142, 42)
         Me.Button2.TabIndex = 2
@@ -57,7 +59,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1457, 440)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
--- vb/program/38_form5_11_2021_12_25/Form1.vb
+++ vb/program/38_form5_11_2021_12_25/Form1.vb
@@ -1,9 +1,5 @@
 ﻿Public Class Form1
-    Dim row, layer As Integer, a() As line '本题使用了图（应该，还没仔细看就得写，原理我想应该差不多）看不懂没关系，跳过就好
-    Structure line
-        Dim num As Integer
-        Dim a() As Integer
-    End Structure
+    Dim row, layer, a(row, layer) As Long
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Button1.Text = "请输入"
@@ -12,13 +8,25 @@
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         row = Val(InputBox("请输入行数", "输入", "3"))
-        ReDim a(row)
-        For i = 1 To row
-            ReDim a(i).a(a(i).num)
-        Next
-        For i = 1 To row
-            For
-        Next
+        layer = row
+        ReDim a(row, layer)
+        TextBox1.Text = ""
+        If row > 26 Then
+            TextBox1.Text = "最大26行"
+        Else
+            For i = 1 To row
+                For j = 1 To i
+                    If i = 1 Then
+                        a(i, j) = 1
+                        TextBox1.Text = TextBox1.Text & a(i, j) & vbTab '" "
+                    Else
+                        a(i, j) = a(i - 1, j - 1) + a(i - 1, j)
+                        TextBox1.Text = TextBox1.Text & a(i, j) & vbTab '" "
+                    End If
+                Next
+                TextBox1.Text = TextBox1.Text & vbCrLf
+            Next
+        End If
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
